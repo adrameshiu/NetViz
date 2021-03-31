c914e81435d7046204b9e72aa175c0032956b830
Aditya <adityaramesh@lishash.com>
2021-03-31 14:58:41 -0400

    representing nodes on the same level in a different graph...handle self loops on nodes

--- network.py
+++ network.py
@@ -129,12 +129,66 @@ def build_network(nodes_heirarchy, all_nodes, network_depth, complete_adjacency_
     edge_colors = nx.get_edge_attributes(G,'color').values()
     edge_weights = nx.get_edge_attributes(G,'weight').values()
 
+    figure_number = 1
+
     #draw networkx map
+    plt.figure(figure_number)
     nx.draw(G, pos=node_pos, node_color=node_color_values, cmap=node_cmap, edge_cmap=edge_cmap, edge_color= edge_colors,width= list(edge_weights), with_labels=True)
     # nx.draw_networkx_edges(
     #     G, node_pos,
     #     connectionstyle="'arc3, rad = 840"  # <-- THIS IS IT
     # )
+
+    # converting to set to get unique values of levels and then converting it back to list of all unique levels
+    all_levels = list(set([node['level'] for node in all_nodes]))
+    print(all_levels)
+
+
+    for level in all_levels:
+        # a separate graph for interneurons
+        has_same_level_graphs = False
+        all_nodes_in_level = [node['label'] for node in all_nodes if node['level']==level]
+        print(all_nodes_in_level)
+        for node1 in all_nodes_in_level:
+            for node2 in all_nodes_in_level:
+                edge_weight = complete_adjacency_matrix.loc[node1,node2]
+                if not math.isnan(edge_weight):
+                    if not has_same_level_graphs:
+                        figure_number = figure_number +1 #new figure found
+                        sub_G = nx.DiGraph()
+                         
+                        plt.figure(figure_number) #make new graph if it has same level graphs
+                        has_same_level_graphs = True
+                    
+                    edge_sign = '+' if edge_weight >=0 else '-'
+                    #edge_color = 'b' if edge_sign == '+' else "g"
+                    edge_color = 0.8 if edge_sign == '+' else 1
+                    abs_edge_weight = abs(edge_weight)
+                    sub_G.add_edge(node1,node2,color=edge_color, weight=abs_edge_weight, alpha=0.5)
+
+        if has_same_level_graphs:
+            node_color_values= []
+            for node in sub_G.nodes():
+                node_color_values.append(1) #color scale value for nodes set to max
+
+            node_colors= nx.get_node_attributes(sub_G,'color').values()
+            node_pos= nx.get_node_attributes(sub_G,'pos')
+            edge_colors = nx.get_edge_attributes(sub_G,'color').values()
+            edge_weights = nx.get_edge_attributes(sub_G,'weight').values()
+            sub_pos = nx.circular_layout(sub_G)  
+            nx.draw(sub_G, sub_pos, node_color=node_color_values, cmap=node_cmap, edge_cmap=edge_cmap, edge_color= edge_colors, width= list(edge_weights), connectionstyle='arc3, rad = 0.1', with_labels=True)
+    # for node1 in all_nodes:
+    #     for node2 in all_nodes:
+    #         node1_label = node1.get("label")
+    #         node2_label = node2.get("label")
+    #         edge_weight = complete_adjacency_matrix.loc[node1_label,node2_label]
+    #         if (node1['type'] == node2['type'] and not math.isnan(edge_weight)):
+    #             has_same_level_graphs = True
+    
+    if has_same_level_graphs:
+        plt.figure(2)
+        interGraph = nx.cycle_graph(4)
+        nx.draw(interGraph)
 
     #storing data in a json for d3
     data = json_graph.node_link_data(G)
